966c03ff8947abfdda82b6ce35347c2a334b820d
Jeff Sanchez <paraglidingjeff@gmail.com>
2015-02-18 15:44:20 +0000

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-
   get 'static_pages/help'
-
   get 'static_pages/about'
   
   resources :microposts
@@ -11,9 +9,6 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
