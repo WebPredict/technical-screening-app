58649944228eba52f32c0dd6c514f988eb75e059
Jeff Sanchez <paraglidingjeff@gmail.com>
2015-02-23 00:26:34 +0000

    more cleanup again

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221203352) do
+ActiveRecord::Schema.define(version: 20150220160355) do
 
   create_table "microposts", force: :cascade do |t|
     t.text     "content"
+    t.string   "picture"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "picture"
   end
 
   add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
@@ -27,17 +27,16 @@ ActiveRecord::Schema.define(version: 20150221203352) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",                        null: false
-    t.datetime "updated_at",                        null: false
     t.string   "password_digest"
     t.string   "remember_digest"
-    t.boolean  "admin",             default: false
+    t.boolean  "admin"
     t.string   "activation_digest"
-    t.boolean  "activated",         default: false
+    t.boolean  "activated"
     t.datetime "activated_at"
     t.string   "reset_digest"
     t.datetime "reset_sent_at"
-    t.string   "interests"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
