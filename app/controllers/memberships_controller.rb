a6e1dfa465794600ed13adea6caa2a847cade865
Jeff Sanchez <paraglidingjeff@gmail.com>
2015-03-31 18:36:26 +0000

    membership fix

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -17,7 +17,7 @@ class MembershipsController < ApplicationController
   end 
   
   def bronze
-    full_key = params[:customer_reference]
+    full_key = params[:ref]
     if full_key.start_with?("4RKzhs3YJMNAAV2v7Zo")
       user_id = full_key["4RKzhs3YJMNAAV2v7Zo".length..full_key.length]
       @user = User.find(user_id)
@@ -35,25 +35,39 @@ class MembershipsController < ApplicationController
   end 
 
   def gold
-    #@user = User.find(params[:id])
-    level = MembershipLevel.find_by(name: "Gold")
-    # TODO we need some sort of security token check here to make sure they've signed up properly
-    security_token = params[:security_token]
-    current_user.membership_level = level
-    current_user.save
-    flash[:success] = "You are now a Gold Membership level TechScreen member!"
-    redirect_to root_url
+    full_key = params[:ref]
+    if full_key.start_with?("4RKzhs3YJMNAAV2v7Zo")
+      user_id = full_key["4RKzhs3YJMNAAV2v7Zo".length..full_key.length]
+      @user = User.find(user_id)
+      level = MembershipLevel.find_by(name: "Gold")
+      @user.membership_level = level
+      @user.activate
+      @user.save
+      log_in @user
+      flash[:success] = "You are now a Gold Membership level TechScreen member!"
+      redirect_to root_url
+    else
+      flash[:error] = "Problem with sign up information..."
+      redirect_to root_url
+    end 
   end 
-
+  
   def platinum
-    #@user = User.find(params[:id])
-    level = MembershipLevel.find_by(name: "Platinum")
-    # TODO we need some sort of security token check here to make sure they've signed up properly
-    security_token = params[:security_token]
-    current_user.membership_level = level
-    current_user.save
-    flash[:success] = "You are now a Platinum Membership level TechScreen member!"
-    redirect_to root_url
+    full_key = params[:ref]
+    if full_key.start_with?("4RKzhs3YJMNAAV2v7Zo")
+      user_id = full_key["4RKzhs3YJMNAAV2v7Zo".length..full_key.length]
+      @user = User.find(user_id)
+      level = MembershipLevel.find_by(name: "Platinum")
+      @user.membership_level = level
+      @user.activate
+      @user.save
+      log_in @user
+      flash[:success] = "You are now a Platinum Membership level TechScreen member!"
+      redirect_to root_url
+    else
+      flash[:error] = "Problem with sign up information..."
+      redirect_to root_url
+    end 
   end 
   
 end
