d9c4b7c33bd348624e28d26d4801468dab07908b
Jeff Sanchez <paraglidingjeff@gmail.com>
2015-04-10 01:03:52 +0000

    fixes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409152204) do
+ActiveRecord::Schema.define(version: 20150410004909) do
 
   create_table "answered_questions", force: :cascade do |t|
     t.string   "answer"
