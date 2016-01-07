9f4765a22e3c43a6aecc618c91fe0bed0f87d38f
Jeff Sanchez <paraglidingjeff@gmail.com>
2016-01-07 23:45:08 +0000

    Renamed the site

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -12,7 +12,7 @@ class MembershipsController < ApplicationController
     # TODO we need some sort of security token check here to make sure they've signed up properly
     @user.membership_level = level
     @user.save
-    flash[:success] = "You are now a " + level.name + " Membership level TechScreen member!"
+    flash[:success] = "You are now a " + level.name + " Membership level T3KSCR33N member!"
     redirect_to root_url
   end 
   
@@ -26,7 +26,7 @@ class MembershipsController < ApplicationController
       @user.activate
       @user.save
       log_in @user
-      flash[:success] = "You are now a Bronze Membership level TechScreen member!"
+      flash[:success] = "You are now a Bronze Membership level T3KSCR33N member!"
       redirect_to root_url
     else
       flash[:error] = "Problem with sign up information..."
@@ -44,7 +44,7 @@ class MembershipsController < ApplicationController
       @user.activate
       @user.save
       log_in @user
-      flash[:success] = "You are now a Gold Membership level TechScreen member!"
+      flash[:success] = "You are now a Gold Membership level T3KSCR33N member!"
       redirect_to root_url
     else
       flash[:error] = "Problem with sign up information..."
@@ -62,7 +62,7 @@ class MembershipsController < ApplicationController
       @user.activate
       @user.save
       log_in @user
-      flash[:success] = "You are now a Platinum Membership level TechScreen member!"
+      flash[:success] = "You are now a Platinum Membership level T3KSCR33N member!"
       redirect_to root_url
     else
       flash[:error] = "Problem with sign up information..."
